8cfb3acb37bf2abf53bf4be1b2082f9cf34503bb
linushahs <suniltraveler2004@gmail.com>
2023-04-14 20:05:07 +0545

    Added react-redux-firebase, redux-firestore package

---
+++ redux-context/authSlice.ts
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState:{
+
+    },
+    reducers:{},
+    extraReducers: (builder)=> {
+    
+    }
+ 
+})
